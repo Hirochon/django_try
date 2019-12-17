87c1b8624722bff7760263fcce8f65e1d24df242
Hirochon <heacet43@gmail.com>
2019-12-18 00:54:37 +0900

    [mod] Modify def to class in view

--- hello/views/index.py
+++ hello/views/index.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.views import View
 
+## 関数を用いたviewの書き方
+"""
 def hello(request):
     params = {
         'title' : 'Index',
         'message' : 'Hello World!!',
     }
     return render(request, 'hello/index.html', params)
+"""
+
+## クラスを用いたviewの書き方
+class HelloView(View):
+    def get(self, request, *args, **kwargs):
+        params = {
+            'title' : 'Index',
+            'message' : 'Hello Class World!!',
+        }
+        return render(request, 'hello/index.html', params)
+
+hello = HelloView.as_view()
