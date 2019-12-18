94975d53244eabb6ec7750b2f705cf59cb115530
Hirochon <heacet43@gmail.com>
2019-12-18 23:04:53 +0900

    [add] Add create function

--- hello/views/create.py
+++ hello/views/create.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import View
+from ..forms import MemberForm
+from ..models import Member
 
 class addView(View):
-    def get(self, request, *args, **kwargs):
-        params = {
+    def __init__(self):
+        self.params = {
             'title' : 'くりえいとぺえじ',
             'message' : 'はろはろクリエイト！！',
+            'form' : MemberForm(),
         }
-        return render(request, 'hello/create.html', params)
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'hello/create.html', self.params)
+    
+    def post(self, request, *args, **kwargs):
+        obj = Member()
+        member = MemberForm(request.POST, instance=obj)
+        member.save()
+        self.params['message'] = 'くりえいとに成功しますぃた。'
+        self.params['form'] = MemberForm(request.POST)
+        return render(request, 'hello/create.html', self.params)
 
 add = addView.as_view()
