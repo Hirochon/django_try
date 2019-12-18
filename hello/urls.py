b63f470e5ffb56c08b52f837597408c97159a424
Hirochon <heacet43@gmail.com>
2019-12-19 05:01:23 +0900

    [add] Add delete function

--- hello/urls.py
+++ hello/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index,create,edit
+from .views import index,create,edit,delete
 
 urlpatterns = [
     path('', index.hello, name='index'),
     path('create/', create.add, name='create'),
     path('morecreate/', create.addmore, name='morecreate'),
     path('edit/<int:num>', edit.edit, name='edit'),
+    path('delete/<int:num>', delete.delete, name='delete'),
 ]
