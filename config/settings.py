5b1ca0d3bd36745ed6169b8bc398fffd8803583e
Hirochon <heacet43@gmail.com>
2019-12-22 06:16:45 +0900

    [add] Add passwordchange.html

--- config/settings.py
+++ config/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
 ##################
 
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
