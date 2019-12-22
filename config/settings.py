a12fa018fe944d325173c2f00883ac30d5d645a0
Hirochon <heacet43@gmail.com>
2019-12-22 10:45:51 +0900

    [add] Add new User model

--- config/settings.py
+++ config/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
