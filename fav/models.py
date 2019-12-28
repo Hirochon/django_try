e5c4219ea57a49b32fa06188163845e7ce79da7a
Hirochon <heacet43@gmail.com>
2019-12-28 18:09:05 +0900

    [mod] Typo

--- fav/models.py
+++ fav/models.py
@@ -15,7 +15,7 @@ class Favorite(models.Model):
     comment = models.TextField(verbose_name='コメント', blank=True, null=True)
     photo = models.ImageField(verbose_name='フォト', blank=True, null=True, upload_to=get_favorite_image_path)
     submitter = models.ForeignKey(settings.AUTH_USER_MODEL, models.PROTECT, verbose_name='投稿者')
-    created_at = models.DateTimeField('作成日時', default=timezone.now)
+    created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
 
     def __str__(self):
         return self.title
