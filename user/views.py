ccdbd6a228a9875e6c9f84ec8339b80459265f9d
Hirochon <heacet43@gmail.com>
2019-12-28 20:14:05 +0900

    [add] Add user application

--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.views import View
 
-# Create your views here.
+class UserView(View):
+    def get(self, request, *args, **kwargs):
+        user_model = get_user_model()
+        users = user_model.objects.all()
+        params = {
+            'users' : users,
+        }
+        return render(request, 'user/user_home.html', params)
